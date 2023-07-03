3fe9cad6a01e637da045f11aaf72e48e98525777
Dzemy <jakubnvt@gmail.com>
2023-07-03 17:36:31 +0200

    init

---
+++ init.ps1
@@ -0,0 +1,24 @@
+# Nastaví aktuální adresář na C:\
+Set-Location -Path 'C:\'
+
+# Vytvoří složku 'temp', pokud neexistuje
+if (!(Test-Path -Path 'temp')) {
+    New-Item -ItemType Directory -Path 'temp'
+}
+
+# Kopíruje všechny soubory a složky z 'app' do 'temp'
+Copy-Item -Path 'app\*' -Destination 'temp' -Recurse
+
+# Přejde do složky 'temp'
+Set-Location -Path 'temp'
+
+# Instaluje balíčky pomocí pnpm
+pnpm install
+
+# Aktualizuje balíčky pomocí pnpm
+pnpm update
+
+# Sestaví projekt pomocí pnpm a Tauri
+pnpm tauri build
+
+powershell
